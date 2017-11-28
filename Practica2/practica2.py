2367616b25bc777c789178538f59ec80eeef0ca7
sanchezismael <sanchezismael@correo.ugr.es>
2017-11-28 08:48:42 +0100

    Añadidos 4 casos de estudio distintos al bucle de algoritmos

--- Practica2/practica2.py
+++ Practica2/practica2.py
@@ -3,7 +3,7 @@ import time
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from sklearn.cluster import KMeans
+from sklearn import cluster
 from sklearn import metrics
 from sklearn import preprocessing
 from math import floor
@@ -17,7 +17,7 @@ prueba = True
 accidentes = pd.read_csv('accidentes_2013.csv')
 
 if(prueba):
-    subconjunto_accidentes = accidentes_2013_orig.sample(len(accidentes_2013_orig)//10)
+    accidentes = accidentes.sample(len(accidentes)//20)
 
 #seleccionar solo accidentes entre las 6 y las 12 de la mañana
 #subset = accidentes.loc[(accidentes['HORA']>=6) & (accidentes['HORA']<=12)]
@@ -27,8 +27,8 @@ if(prueba):
 
 #seleccionar variables de interés para clustering
 #usadas = ['HORA', 'DIASEMANA', 'TOT_VICTIMAS', 'TOT_MUERTOS', 'TOT_HERIDOS_GRAVES', 'TOT_HERIDOS_LEVES', 'TOT_VEHICULOS_IMPLICADOS']
-usadas = ['TOT_VICTIMAS', 'TOT_MUERTOS', 'TOT_HERIDOS_GRAVES', 'TOT_HERIDOS_LEVES', 'TOT_VEHICULOS_IMPLICADOS']
-X = subset[usadas]
+# usadas = ['TOT_VICTIMAS', 'TOT_MUERTOS', 'TOT_HERIDOS_GRAVES', 'TOT_HERIDOS_LEVES', 'TOT_VEHICULOS_IMPLICADOS']
+# X = subset[usadas]
 #seleccionar accidentes de tipo 'colisión de vehículos'
 subset1 = accidentes[accidentes['TIPO_ACCIDENTE'].str.contains("Colisión de vehículos")]
 caso_estudio1 = ['TOT_VICTIMAS', 'TOT_MUERTOS', 'TOT_HERIDOS_GRAVES', 'TOT_HERIDOS_LEVES', 'TOT_VEHICULOS_IMPLICADOS']
@@ -43,18 +43,45 @@ subset4 = accidentes[accidentes['TIPO_ACCIDENTE'].str.contains("Salida de la ví
 caso_estudio4 = ['TOT_VICTIMAS', 'TOT_MUERTOS', 'TOT_HERIDOS_GRAVES', 'TOT_HERIDOS_LEVES', 'TOT_VEHICULOS_IMPLICADOS']
 X4 = subset4[caso_estudio4]
 
+casos_de_estudio = [X1,X2,X3,X4]
 
-for x in (X1,X2,X3,X4):
-    X_normal = preprocessing.normalize(X, norm='l2')
+k_means = cluster.KMeans(init='k-means++',n_clusters=4, n_init=5)
+mbkm = cluster.MiniBatchKMeans(n_clusters=4)
+ms = cluster.MeanShift()
+spectral = cluster.SpectralClustering(n_clusters=4)
+affinity_propagation = cluster.AffinityPropagation()
+dbscan = cluster.DBSCAN(eps=0.1)
+birch = cluster.Birch(n_clusters=4,threshold=0.1)
+ward = cluster.AgglomerativeClustering(n_clusters=100, linkage='ward')
 
 clustering_algorithm = (
-    ('k-means',k_means)
+    ('k-means',k_means),
+    ('MiniBatkMeans',mbkm),
+    ('MeanShift',ms),
+    ('DBSCAN',dbscan),
+    ('Birch',birch),
+    ('SpectralClustering',spectral),
+    ('Ward',ward)
 )
 
-for name,algotithm in clustering_algorithm:
-    print('{:19s}'.format(name),end='')
-    t = t.time.time()
-    cluster_predict = algorithms.fit_predict(X_normal)
-    k = len(set(cluster_predict))
-    print(': k: {3.0f}, '.format(k),end='')
-    print("{:6.2f}")
+for i,X in enumerate(casos_de_estudio):
+    print("\nCaso de estudio "+str(i+1)+":\n")
+    X_normal = preprocessing.normalize(X, norm='l2')
+
+    for name,algorithm in clustering_algorithm:
+        print('{:19s}'.format(name),end='')
+        t = time.time()
+        cluster_predict = algorithm.fit_predict(X_normal)
+        tiempo = time.time() - t
+        k = len(set(cluster_predict))
+        print(": k: {:3.0f}, ".format(k),end='')
+        print("{:6.2f} segundos, ".format(tiempo),end='')
+        if(k>1) and (name is not 'Ward'):
+            metric_CH = metrics.calinski_harabaz_score(X_normal, cluster_predict)
+
+            metric_SH = metrics.silhouette_score(X_normal, cluster_predict,metric = 'euclidean',sample_size=floor(0.1*len(X1)),random_state=123456)
+        else:
+            metric_CH = 0
+            metric_SH = 0
+        print("CH Index: {:8.9f}, ".format(metric_CH),end='')
+        print("SC: {:.5f}".format(metric_SH))
